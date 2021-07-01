5bfdd42787375bbac0fe9c0315f96508891b6b88
Andrew Lamoureux <andrew@vector35.com>
2021-07-01 19:19:27 -0400

    Add Hey! That's my fish!

---
+++ hey_thats_my_fish/build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+montage \
+1Fish.jpg 1Fish.jpg 1Fish.jpg 1Fish.jpg 1Fish.jpg \
+1Fish.jpg 1Fish.jpg 1Fish.jpg 1Fish.jpg 1Fish.jpg \
+1Fish.jpg 1Fish.jpg 1Fish.jpg 1Fish.jpg 1Fish.jpg \
+1Fish.jpg 1Fish.jpg 1Fish.jpg 1Fish.jpg 1Fish.jpg \
+1Fish.jpg 1Fish.jpg 1Fish.jpg 1Fish.jpg 1Fish.jpg \
+1Fish.jpg 1Fish.jpg 1Fish.jpg 1Fish.jpg 1Fish.jpg \
+2Fish.jpg 2Fish.jpg 2Fish.jpg 2Fish.jpg 2Fish.jpg \
+2Fish.jpg 2Fish.jpg 2Fish.jpg 2Fish.jpg 2Fish.jpg \
+2Fish.jpg 2Fish.jpg 2Fish.jpg 2Fish.jpg 2Fish.jpg \
+2Fish.jpg 2Fish.jpg 2Fish.jpg 2Fish.jpg 2Fish.jpg \
+3Fish.jpg 3Fish.jpg 3Fish.jpg 3Fish.jpg 3Fish.jpg \
+3Fish.jpg 3Fish.jpg 3Fish.jpg 3Fish.jpg 3Fish.jpg \
+-geometry +2+2 stickersheet.png
